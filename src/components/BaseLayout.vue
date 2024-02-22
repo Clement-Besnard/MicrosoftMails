a64c645e5d2c66c36bba0ff2efb363f767436010
TonPotoMid <hamiddiaw@gmail.com>
2024-02-22 19:23:32 +0100

    ajout des boutons Home Mail et Déconnexion, en cours de modifs pour qu'ils marchent

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -2,11 +2,21 @@
   <div>
     <header class="green-header">
       <div class="header-container">
-        <h1>VueJS</h1>
-        <span class='username' v-if="username">{{ username }}</span>
-        <span v-else>
-          <SigninButton @signin-success="onSignInSuccess" />
-        </span>
+        <div class="buttons-container">
+          <h1>VueJS</h1>
+          
+          <BaseButton @click="goHome" color="primary">Home</BaseButton>
+          <BaseButton @click="goMail" color="primary">Mail</BaseButton>
+          <BaseButton @click="logout" color="primary">Déconnexion</BaseButton>
+        </div>
+        
+        <div>
+          
+          <span class='username' v-if="username">{{ username }}</span>
+          <span v-else>
+            <SigninButton @signin-success="onSignInSuccess" />
+          </span>
+        </div>
       </div>
     </header>
     <main>
@@ -14,27 +24,55 @@
     </main>
 
     <footer>
-      <p style="color: #ffffff; ">Copyright © 2023 My App</p>
+      <p style="color: #ffffff;"></p>
     </footer>
   </div>
 </template>
 
 <script>
 import SigninButton from "@/components/SigninButton.vue";
+import BaseButton from "@/components/BaseButton.vue";
 
 export default {
   name: "BaseLayout",
   components: {
     SigninButton,
+    BaseButton,
   },
   props: ['username'],
   methods: {
     onSignInSuccess(username) {
       this.$emit('set-username', username);
-    }
+    },
+    goHome() {
+      // Logic for navigating to the home page
+      console.log("Navigating to the home page");
+    },
+    goMail() {
+      // Logic for navigating to the mail page
+      console.log("Navigating to the mail page");
+    },
+    logout() {
+      // Logic for user logout
+      console.log("User logout");
+      // You can add further logic here, such as clearing tokens or redirecting
+    },
   },
   mounted() {
-    console.log('HomePage mounted. Username:', this.username);
+    console.log('BaseLayout mounted. Username:', this.username);
   },
 };
 </script>
+
+<style scoped>
+.buttons-container {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center; /* Aligner les éléments verticalement au centre */
+}
+
+/* Ajout de styles pour le bouton de déconnexion */
+.base-button--logout {
+  margin-right: 10px;
+}
+</style>
