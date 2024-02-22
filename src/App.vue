ba08f6e59da08d13d657a615d8e3533d1569faee
Clément <clems.2048@gmail.com>
2024-02-22 17:18:53 +0100

    router.js issue resolved (App.vue fix) + Delete Test.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <BaseLayout>
+    <RouterView></RouterView>
   </BaseLayout>
 </template>
 
--- src/router.js
+++ src/router.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './components/HomePage.vue';
 import AuthCallback from './components/AuthCallback.vue';
-import Test from './components/Test.vue';
 
-console.log(Test)
+
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/test', component: Test },
   { path: '/auth/callback', component: AuthCallback } // Ajoutez la route pour la page de retour d'authentification
 ];
 
