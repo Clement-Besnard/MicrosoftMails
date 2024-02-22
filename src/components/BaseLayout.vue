485302c84c1b8b6de50959c95ba909b04bc78fc9
Mathieu9488 <mathieucrespin94@gmail.com>
2024-02-22 22:01:09 +0100

    Accès a la page Mails restreint si l'utilisateur n'est pas connecté (message d'erreur)

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -46,6 +46,10 @@ export default {
       console.log("Navigating to the home page");
     },
     goMail() {
+      if (!this.username) {
+        alert("Vous devez être connecté pour accéder à la page des e-mails.");
+        return;
+      }
       this.$router.push('/mails');
       console.log("Navigating to the mail page");
     },
