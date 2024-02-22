b4228e6be0460cc9e7afaf364d28fb7e9392f0ea
Mathieu9488 <mathieucrespin94@gmail.com>
2024-02-22 19:59:44 +0100

    Suppression des éléments de HomePage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,3 @@
 <template>
 <h1></h1>
 </template>
-
-  <h1 class="title"></h1>
-  <h2>Bienvenue sur MicrosoftMails ! Ici, vous pourrez consulter vos mails en vous connectant ci dessous : </h2>
-  
- 
