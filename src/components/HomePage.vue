03f33eca12dc8f7392dfb34b437467452dcfedc5
Mathieu9488 <mathieucrespin94@gmail.com>
2024-02-22 19:36:12 +0100

    resolve error

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,14 +1,8 @@
 <template>
-<h1><center></center></h1>
+<h1></h1>
 </template>
 
   <h1 class="title"></h1>
   <h2>Bienvenue sur MicrosoftMails ! Ici, vous pourrez consulter vos mails en vous connectant ci dessous : </h2>
-  </template>
   
-  <style>
-  .title{
-    padding-top: 5%;
-    font-size: 50px;
-  }
-  </style>
+ 
